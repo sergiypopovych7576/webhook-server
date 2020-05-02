0144b84acec34ac17a61ba6412216d049fffbcb9
Sergiy Popovych <popovychsergiy76@gmail.com>
2020-05-02 13:50:54 +0300

    Fixed script

--- scripts/discord-bot-deploy.sh
+++ scripts/discord-bot-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/slink7576/hosting
 cd discord-bot
-git pull
-npm install
-cp -f ./src/settings-deploy.json ./src/settings.json
+git pull && 
+npm install && 
+cp -f ./src/settings-deploy.json ./src/settings.json &&
 tsc
